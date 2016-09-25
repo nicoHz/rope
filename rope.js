2293d93224602d452b4c10da76cee7bab0401c49
nicoHz <nicoHz@posteo.de>
2016-09-25 19:14:44 +0200

    Connect points with lines

--- rope.js
+++ rope.js
@@ -2,12 +2,17 @@
 function points(x, y, radius, numberOfPoints)
 {
     for(var i=0; i<numberOfPoints; i++) {
-	ctx.beginPath();
-    ctx.arc(x * i + x, y, radius, 0, 2 * Math.PI, true);
-	console.log(i);
-    ctx.fill();
+		ctx.beginPath();
+		ctx.arc(x * i + x, y, radius, 0, 2 * Math.PI, true);
+		console.log(i);
+		ctx.fill();
+
+		ctx.moveTo(x,y);
+		ctx.lineTo(x * i + x, y);
+		ctx.stroke();
 	}
 }
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
