69a7f0e043ad3679f74e14259b6b2747e481f00a
nicoHz <nicoHz@posteo.de>
2016-09-27 19:18:55 +0200

    Single control for x and y coordinate

--- rope.js
+++ rope.js
@@ -1,19 +1,23 @@
 
-function points(x, y, radius, numberOfPoints)
+function points(x_control, y_control, radius, numberOfPoints)
 {
-    for(var i=0; i<numberOfPoints; i++) {
+	for(var i=0; i<numberOfPoints; i++) {
+		var x = x_control[i];
+		var y = y_control[i]; 	
+		
 		ctx.beginPath();
-		ctx.arc(x * i + x, y, radius, 0, 2 * Math.PI, true);
-		console.log(i);
+		ctx.arc(x + x, y, radius, 0, 2 * Math.PI, true);
 		ctx.fill();
 
-		ctx.moveTo(x,y);
-		ctx.lineTo(x * i + x, y);
+		ctx.moveTo(x, y);
+		ctx.lineTo(x, y);
 		ctx.stroke();
 	}
 }
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x_control = [20,40,200,250,260];
+var y_control = [200, 400, 300, 240, 40];
 
-points(70,60,6,5);
+points(x_control, y_control,6,5);
