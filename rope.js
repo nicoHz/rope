2459cf851ab478f8b01d92f22beba9ff35de1d3d
nicoHz <nicoHz@posteo.de>
2016-10-10 07:49:58 +0200

    Bugfixed sinus function: pushing values to an array and return it to the function call

--- rope.js
+++ rope.js
@@ -1,12 +1,11 @@
 
-function drawSine(numberOfCircles, horizontalPositions, verticalPositions, radius)
-{
-/*	if (horizontalPositions.length !== verticalPositions.length) {
+function drawRope(horizontalPositions, verticalPositions, radius){
+	if (horizontalPositions.length !== verticalPositions.length) {
 		throw new Error("horizontalPositions.length does not match verticalPositions.length");
 	}
-*/
+
 	var oldx, oldy;
-	for(var i=0; i<numberOfCircles; i++) {
+	for(var i=0; i<=360; i++) {
 		var x = horizontalPositions[i];
 		var y = verticalPositions[i];
 		
@@ -16,7 +15,7 @@ function drawSine(numberOfCircles, horizontalPositions, verticalPositions, radiu
 
 		if (i !== 0) {
 			ctx.moveTo(x, y); 
-			ctx.lineTo(oldx, oldy);
+			ctx.lineTo(oldx, oldy); // das objekt ctx hat die eigenschaft lineTo, die eine funktion ist. die funktion lineTo wird mir zwei parametern aufgerufen: werte oldx, oldy 
 			ctx.stroke();
 		}
 
@@ -25,13 +24,31 @@ function drawSine(numberOfCircles, horizontalPositions, verticalPositions, radiu
 	}
 }
 
+function sinusShape(xps) {	
+	var yps = []; 
+	for (var i=0; i< xps.length; i++) {
+
+		var y = Math.sin(xps[i] * Math.PI/180) * -120 + 180;
+		yps.push(y);
+		/* 
+		y = sin(x * sx + tx) * sy + ty
+		sx: in x richtung skaliert = frequnz auf x achse 
+		sy: skalierung der amplitude
+		ty: verschiebt nach oben bzw. unten
+		tx: transmission links, rechts
+		*/
+	}
+	console.log('yps=', yps); 
+
+	return yps;
+}
 
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-var t = 50;
-var x_positions = t + 5; 
-var y_positions = Math.sin(t + 0.5); 
+var x_positions = [20, 40, 60, 100, 130, 140];
+var y_positions = sinusShape(x_positions);// Übergabe des Arrayis x_psition an function sinusShape und zuweisung des return-wertes von sinusShape an y_positions.
+console.log(y_positions);
 
-drawSine(100, x_positions, y_positions,6);
+drawRope(x_positions, y_positions, 6);
 
